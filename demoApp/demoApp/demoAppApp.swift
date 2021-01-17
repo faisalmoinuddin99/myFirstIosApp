7724451738e0dd7417aba7bbc72a731d78a309e5
faisal <faisal25march99@gmail.com>
2021-01-17 16:15:33 +0530

    first commit

---
+++ demoApp/demoApp/demoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoAppApp.swift
+//  demoApp
+//
+//  Created by ZAREEN NAUSHAD on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct demoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
