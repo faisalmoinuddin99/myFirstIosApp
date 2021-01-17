ee9ae7b011a1d9ee07c5084253f0fa1ce0f652b9
faisal <faisal25march99@gmail.com>
2021-01-17 17:10:06 +0530

    Done with Home Screen

--- demoApp/demoApp/ContentView.swift
+++ demoApp/demoApp/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            VStack{
+                Text("Courses")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                ScrollView(.horizontal){
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+                           spacing: 20
+                    ){
+                        ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                            card()
+                        }
+                    }
+                }
+            }
+            VStack{
+                Text("Bundles")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                ScrollView(.horizontal){
+                    HStack{
+                        ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                            card()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
